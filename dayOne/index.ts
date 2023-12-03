f47267bbef5462b74e03c2c49ffc2407150dca77
Grace Bennett <grace.bennett@shopify.com>
2023-12-03 15:24:09 -0500

    Day 3 and prettier

--- dayOne/index.ts
+++ dayOne/index.ts
@@ -2,76 +2,84 @@ import { PathOrFileDescriptor } from "fs";
 import { parseInput } from "../utilities/parser";
 
 export function run(filename: PathOrFileDescriptor) {
-    const arr = parseInput(filename);
+  const arr = parseInput(filename);
 
-    const numberArray = arr.map(calibrationText => {
-        const first = returnFirstDigit(calibrationText.split(''));
-        const last = returnFirstDigit(calibrationText.split('').reverse());
-        return parseInt(first+last);
-    });
-    const partOne = numberArray.reduce((a,b) => a+b);
+  const numberArray = arr.map((calibrationText) => {
+    const first = returnFirstDigit(calibrationText.split(""));
+    const last = returnFirstDigit(calibrationText.split("").reverse());
+    return parseInt(first + last);
+  });
+  const partOne = numberArray.reduce((a, b) => a + b);
 
-    console.log("Part one: ", partOne);
+  console.log("Part one: ", partOne);
 
-    const spelledNumberArray = arr.map(calibrationText => {
-        const first = returnFirstSpelledDigit(calibrationText);
-        const last = returnLastSpelledDigit(calibrationText.split('').reverse().join(''));
-        return parseInt(first+last);
-    });
-    const partTwo = spelledNumberArray.reduce((a,b) => a+b);
+  const spelledNumberArray = arr.map((calibrationText) => {
+    const first = returnFirstSpelledDigit(calibrationText);
+    const last = returnLastSpelledDigit(
+      calibrationText.split("").reverse().join("")
+    );
+    return parseInt(first + last);
+  });
+  const partTwo = spelledNumberArray.reduce((a, b) => a + b);
 
-    console.log("Part two: ", partTwo);
-
+  console.log("Part two: ", partTwo);
 }
 
 function returnFirstDigit(text: string[]): string {
-    return text.find(character => character.match('[0-9]')) || '0';
+  return text.find((character) => character.match("[0-9]")) || "0";
 }
 
 function returnFirstSpelledDigit(text: string): string {
-    const positions = [
-        {id: '1', value: findNumberPosition(text, 'one' ,'1')},
-        {id: '2', value: findNumberPosition(text, 'two' ,'2')},
-        {id: '3', value: findNumberPosition(text, 'three' ,'3')},
-        {id: '4', value: findNumberPosition(text, 'four' ,'4')},
-        {id: '5', value: findNumberPosition(text, 'five' ,'5')},
-        {id: '6', value: findNumberPosition(text, 'six' ,'6')},
-        {id: '7', value: findNumberPosition(text, 'seven' ,'7')},
-        {id: '8', value: findNumberPosition(text, 'eight' ,'8')},
-        {id: '9', value: findNumberPosition(text, 'nine' ,'9')}
-    ].filter(number => number.value !== -1).sort((a,b) => {
-        if (a.value > b.value) return 1;
-        return -1;
+  const positions = [
+    { id: "1", value: findNumberPosition(text, "one", "1") },
+    { id: "2", value: findNumberPosition(text, "two", "2") },
+    { id: "3", value: findNumberPosition(text, "three", "3") },
+    { id: "4", value: findNumberPosition(text, "four", "4") },
+    { id: "5", value: findNumberPosition(text, "five", "5") },
+    { id: "6", value: findNumberPosition(text, "six", "6") },
+    { id: "7", value: findNumberPosition(text, "seven", "7") },
+    { id: "8", value: findNumberPosition(text, "eight", "8") },
+    { id: "9", value: findNumberPosition(text, "nine", "9") },
+  ]
+    .filter((number) => number.value !== -1)
+    .sort((a, b) => {
+      if (a.value > b.value) return 1;
+      return -1;
     });
-    return positions[0].id;
-
+  return positions[0].id;
 }
 
 function returnLastSpelledDigit(text: string): string {
-    const positions = [
-        {id: '1', value: findNumberPosition(text, 'eno' ,'1')},
-        {id: '2', value: findNumberPosition(text, 'owt' ,'2')},
-        {id: '3', value: findNumberPosition(text, 'eerht' ,'3')},
-        {id: '4', value: findNumberPosition(text, 'ruof' ,'4')},
-        {id: '5', value: findNumberPosition(text, 'evif' ,'5')},
-        {id: '6', value: findNumberPosition(text, 'xis' ,'6')},
-        {id: '7', value: findNumberPosition(text, 'neves' ,'7')},
-        {id: '8', value: findNumberPosition(text, 'thgie' ,'8')},
-        {id: '9', value: findNumberPosition(text, 'enin' ,'9')}
-    ].filter(number => number.value !== -1).sort((a,b) => {
-        if (a.value > b.value) return 1;
-        return -1;
+  const positions = [
+    { id: "1", value: findNumberPosition(text, "eno", "1") },
+    { id: "2", value: findNumberPosition(text, "owt", "2") },
+    { id: "3", value: findNumberPosition(text, "eerht", "3") },
+    { id: "4", value: findNumberPosition(text, "ruof", "4") },
+    { id: "5", value: findNumberPosition(text, "evif", "5") },
+    { id: "6", value: findNumberPosition(text, "xis", "6") },
+    { id: "7", value: findNumberPosition(text, "neves", "7") },
+    { id: "8", value: findNumberPosition(text, "thgie", "8") },
+    { id: "9", value: findNumberPosition(text, "enin", "9") },
+  ]
+    .filter((number) => number.value !== -1)
+    .sort((a, b) => {
+      if (a.value > b.value) return 1;
+      return -1;
     });
-    return positions[0].id;
+  return positions[0].id;
 }
 
-function findNumberPosition(text: string, match: string, match2: string): number {
-    const spelled = text.indexOf(match);
-    const numeric = text.indexOf(match2);
+function findNumberPosition(
+  text: string,
+  match: string,
+  match2: string
+): number {
+  const spelled = text.indexOf(match);
+  const numeric = text.indexOf(match2);
 
-    if (spelled === -1) return numeric;
-    if (numeric === -1) return spelled;
-    return Math.min(text.indexOf(match), text.indexOf(match2));
+  if (spelled === -1) return numeric;
+  if (numeric === -1) return spelled;
+  return Math.min(text.indexOf(match), text.indexOf(match2));
 }
 
 run("dayOne/input.txt");
---
+++ dayThree/index.ts
@@ -0,0 +1,139 @@
+import { PathOrFileDescriptor } from "fs";
+import { parseInput } from "../utilities/parser";
+
+interface EngineNumber {
+  xMin: number;
+  xMax: number;
+  y: number;
+  value: string;
+}
+
+interface Symbol {
+  x: number;
+  y: number;
+  value: string;
+}
+
+export function run(filename: PathOrFileDescriptor) {
+  const arr = parseInput(filename);
+
+  const [symbols, gears, engineNumbers] = parseEngine(arr);
+
+  const partNumbers = filterEngineNumbers(symbols, engineNumbers);
+
+  const partOne = partNumbers.reduce((a, b) => a + parseInt(b.value, 10), 0);
+  console.log("Part 1: ", partOne);
+
+  const gearRatios = getGearRatios(gears, engineNumbers);
+
+  const partTwo = gearRatios.reduce((acc, ratio) => acc + ratio);
+  console.log("Part 2: ", partTwo);
+}
+
+export function getGearRatios(
+  gears: Symbol[],
+  partNumbers: EngineNumber[]
+): number[] {
+  return gears.reduce((acc, gear) => {
+    const adjacentPartNumbers: number[] = [];
+    partNumbers.forEach((partNumber) => {
+      const adjacentOnY =
+        partNumber.y >= gear.y - 1 && partNumber.y <= gear.y + 1;
+      const adjacentOnX =
+        (partNumber.xMax >= gear.x - 1 && partNumber.xMax <= gear.x + 1) ||
+        (partNumber.xMin >= gear.x - 1 && partNumber.xMin <= gear.x + 1);
+      if (adjacentOnX && adjacentOnY) {
+        adjacentPartNumbers.push(parseInt(partNumber.value));
+      }
+    });
+    if (adjacentPartNumbers.length === 2) {
+      acc.push(adjacentPartNumbers[0] * adjacentPartNumbers[1]);
+    }
+    return acc;
+  }, [] as number[]);
+}
+
+export function filterEngineNumbers(
+  symbols: string[][],
+  engineNumbers: EngineNumber[]
+): EngineNumber[] {
+  return engineNumbers.filter((engineNumber) => {
+    for (let y = engineNumber.y - 1; y <= engineNumber.y + 1; y++) {
+      if (y < 0 || y >= symbols.length) {
+        continue;
+      }
+      for (let x = engineNumber.xMin - 1; x <= engineNumber.xMax + 1; x++) {
+        if (x < 0 || x >= symbols[y].length) {
+          continue;
+        }
+        if (symbols[y][x]) {
+          return true;
+        }
+      }
+    }
+  });
+}
+
+export function parseEngine(
+  arr: string[]
+): [string[][], Symbol[], EngineNumber[]] {
+  const symbols: string[][] = new Array(arr.length);
+  const gears: Symbol[] = [];
+  const engineNumbers: EngineNumber[] = [];
+
+  arr.forEach((line, lineIndex) => {
+    symbols[lineIndex] = new Array(line.length);
+    let currY = -1;
+    let currXMin = -1;
+    let currVal = "";
+
+    line.split("").forEach((character, characterIndex) => {
+      if (character !== ".") {
+        if (character.match("[0-9]")) {
+          currY = lineIndex;
+          currVal = currVal !== "" ? currVal + character : character;
+          if (currXMin < 0) {
+            currXMin = characterIndex;
+          }
+        } else {
+          if (currXMin !== -1) {
+            engineNumbers.push({
+              xMin: currXMin,
+              xMax: characterIndex - 1,
+              y: currY,
+              value: currVal,
+            });
+            currY = -1;
+            currVal = "";
+            currXMin = -1;
+          }
+          symbols[lineIndex][characterIndex] = character;
+          if (character === "*") {
+            gears.push({ y: lineIndex, x: characterIndex, value: character });
+          }
+        }
+      } else if (currXMin !== -1) {
+        engineNumbers.push({
+          xMin: currXMin,
+          xMax: characterIndex - 1,
+          y: currY,
+          value: currVal,
+        });
+        currY = -1;
+        currVal = "";
+        currXMin = -1;
+      }
+    });
+    if (currXMin !== -1) {
+      engineNumbers.push({
+        xMin: currXMin,
+        xMax: line.length - 1,
+        y: currY,
+        value: currVal,
+      });
+    }
+  });
+  return [symbols, gears, engineNumbers];
+}
+
+run("dayThree/input.txt");
